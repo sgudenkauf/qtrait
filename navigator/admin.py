3bd7061d9b8f5b1f35527676a0e9ba577d2cbb84
Tomi <imsommerteetrinker@yahoo.de>
2024-06-26 11:43:36 +0200

    add mptt

--- navigator/admin.py
+++ navigator/admin.py
@@ -4,6 +4,11 @@ from .models import *
 
 # Register your models here.
 
+
+class BockAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Project)
 admin.site.register(Service)
 admin.site.register(Feature)
